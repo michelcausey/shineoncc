32a223fada8f141d0ce5fac314dc70b0e40a1fde
MiChel Causey <michel.causey@gmail.com>
2019-09-08 17:42:55 -0400

    add basics

---
+++ shine-on-cc/src/components/Fees-Insurance/Fees.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Fees.css';
+
+export default function Fees() {
+
+    return (
+    <div >
+
+        
+    </div>
+    );
+}
---
+++ shine-on-cc/src/components/Legal/Legal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Legal.css';
+
+export default function Legal() {
+
+    return (
+    <div >
+
+        
+    </div>
+    );
+}
